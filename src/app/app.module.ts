c4e9bc944fb2cccf50ed7a14963dc1c1cdd341b2
11vishakha <vishakhamhasaye04@gmail.com>
2023-03-23 14:43:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SellerAuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
